234f1869ca585e6fb6438a296a44e928f66998fa
ClugstonM <michael.j.clugston@gmail.com>
2025-05-05 22:57:27 -0400

    Add code

---
+++ HW9/helloworld.asm
@@ -0,0 +1,22 @@
+	;; helloworld.asm
+	;; Michael Clugston
+	;; CMSC313@1430
+	;; Hello world program
+
+SECTION .data
+	msg db 'Hello World!', 0Ah
+
+SECTION .text
+	global _start
+
+_start:
+
+	mov edx, 13
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 80h
+
+	mov ebx, 0
+	mov eax, 1
+	int 80h
